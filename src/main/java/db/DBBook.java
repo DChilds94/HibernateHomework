1d4b8f15ab1333dd069554c903d7cc7e87df5626
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-14 16:31:48 +0100

    save books

--- src/main/java/db/DBBook.java
+++ src/main/java/db/DBBook.java
@@ -1,10 +1,31 @@
 package db;
 
+import models.Author;
+import models.Book;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class DBBook {
 
     private static Session session;
     private static Transaction transaction;
+
+
+    public static void save(Book book){
+        session = HibernateUtil.getSessionfactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+        } catch (HibernateException e){
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
 }
